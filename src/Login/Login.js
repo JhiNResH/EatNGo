d135de9cd174aaf89edea798028a9c95ed4681ec
Jerry Chen <jhinresh@JerrydeMBP.dhost.uta.edu>
2023-11-27 18:32:20 -0600

    new

---
+++ src/Login/Login.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import image from '../../assets/images/OPOS.gif'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>EatNGo</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default Login
+
