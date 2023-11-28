3476fee4dc02a28567f922f3cbc656bda1441d82
Jerry Chen <jhinresh@JerrydeMBP.dhost.uta.edu>
2023-11-27 20:48:59 -0600

    Login/User

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,16 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import image from '../../assets/images/OPOS.gif'
+import { StyleSheet, Text, View, Image, useWindowDimensions } from 'react-native'
+import React, { useState } from 'react'
+import User from '../../components/customInput/User';
+import Logo from '../../assets/images/possible.jpg';
+
 
 const Login = () => {
-  return (
-    <View>
-      <Text>EatNGo</Text>
-    </View>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-const styles = StyleSheet.create({})
+  const { height } = useWindowDimensions();
+
+  return (
+    <View style={styles.root}>
+      <Image
+        source={Logo}
+        style={[styles.logo, { height: height * 0.3 }]}
+        resizeMode='contain'
+      />
+      <User
+        placeholder="Username"
+        value={username}
+        setValue={setUsername} />
+      <User
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        //secureTextEntry={true} => It's the same
+        secureTextEntry
+      />
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+  }
+})
 
 export default Login
 
